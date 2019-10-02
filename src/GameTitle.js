c090ee7d81ea2a0e8cee054f7e67bf87cf79c3c9
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-02 09:38:23 -0400

    Add GameTitle stylesheet

--- src/GameTitle.js
+++ src/GameTitle.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './GameTitle.css';
 
 class GameTitle extends React.Component {
 
     render(){
-        
+
         return(
             <h1>{this.props.title}</h1>
         )
