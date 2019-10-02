65d23aee987fda55684b2c3e34e85f4220740b58
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-01 20:44:06 -0400

    Add Title to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Title from "./Title";
 class App extends React.Component {
     
     render(){
 
         return(
             
+            <Title />
         );
     }    
 }
