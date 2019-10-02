bea9f7050cd1a1fb04c10ea145dbde8eacc3d788
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-02 10:09:08 -0400

    Render board on GameBoard

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
+
 class GameBoard extends React.Component {
+
+    renderColumns(lengh){
+        
+        let collumns = [];
+
+        for(let i = 0; i < lengh; i++){
+            collumns.push(<tr></tr>);
+        }
+
+        return collumns;
+    }
+
+    renderRows(lengh) {
+
+        let rows = [];
+
+        for (let i = 0; i < lengh; i++){
+            rows.push(<td>{this.renderColumns(lengh)}</td>);
+        }
+
+        return rows;
+    }
 
     render() {
 
-        return();
+        const lengh = parseInt(this.props.lengh);
+
+        return(
+            <table>
+                {this.renderRows(lengh)}
+            </table>
+        );
     }
 }
 
