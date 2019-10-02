b76e1969677f30c7eb2cae1a66c8b320114a65cd
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-02 10:08:33 -0400

    Add GameBoard to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import GameTitle from "./GameTitle";
+import GameBoard from "./GameBoard";
 class App extends React.Component {
     
     render(){
 
         return(
-            <GameTitle
-                title="the game of life"/>
-                
+
+            <React.Fragment>
+                <GameTitle
+                    title="the game of life"/>
+
+                <GameBoard lengh="4"/>
+            </React.Fragment>
+
         );
     }    
 }
