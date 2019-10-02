a114e39a183d4d7551e0175e2c52d61f406594c4
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-02 11:31:39 -0400

    Enhance rows and collumns render logic

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -3,23 +3,35 @@ import "./GameBoard.css";
 
 class GameBoard extends React.Component {
 
-    renderColumns(lengh){
+    renderColumns(lengh, x, y){
         
         let collumns = [];
+        let gameStatus = "game-col";
 
         for(let i = 0; i < lengh; i++){
-            collumns.push(<tr></tr>);
+
+            if (x && y === i)
+                gameStatus += " is-alive";
+
+            collumns.push(<td className={gameStatus}></td>);
+            gameStatus = "game-col";
         }
 
         return collumns;
     }
 
-    renderRows(lengh) {
+    renderRows(lengh, x, y) {
 
         let rows = [];
 
         for (let i = 0; i < lengh; i++){
-            rows.push(<td>{this.renderColumns(lengh)}</td>);
+            
+            if (x === i)
+                x = true;
+            else
+                x = false;
+
+            rows.push(<tr className="game-row">{this.renderColumns(lengh, x, y)}</tr>);
         }
 
         return rows;
@@ -30,8 +42,8 @@ class GameBoard extends React.Component {
         const lengh = parseInt(this.props.lengh);
 
         return(
-            <table>
-                {this.renderRows(lengh)}
+            <table className="game-board">
+                {this.renderRows(lengh, 0, 0)}
             </table>
         );
     }
