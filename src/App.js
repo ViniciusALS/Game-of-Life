26bfe33a1fe22db8a84ebb126be2bd5df2572517
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-03 01:56:40 -0400

    Passes variable length to method

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
         };
     }
 
-    createBoardArray(){
+    createBoardArray(length = this.state.boardlength){
 
         let collumns = [];
         let board = [];
