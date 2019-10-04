71bab1a87a109744e9d1c449c9ab51fdcdcb9073
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-04 02:09:32 -0400

    Change board rendering methods to a single method that can handle two dimentional arrays

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -2,50 +2,34 @@ import React from 'react';
 import "./stylesheets/GameBoard.css";
 
 class GameBoard extends React.Component {
+ 
+    renderBoard(){
 
-    renderColumns(length, x, y){
-        
-        let collumns = [];
-        let gameStatus = "game-col";
+        let boardTable = this.props.board.map(rows => {
+            rows = rows.map(cells => {
+                if (cells)
+                    return(<td className="game-cell is-alive"/>);
+                else
+                    return(<td className="game-cell"/>);
+            });
 
-        for(let i = 0; i < length; i++){
-
-            if (x && y === i)
-                gameStatus += " is-alive";
-
-            collumns.push(<td className={gameStatus}></td>);
-            gameStatus = "game-col";
-        }
-
-        return collumns;
-    }
-
-    renderRows(length, x, y) {
-
-        let rows = [];
-
-        for (let i = 0; i < length; i++){
-            
-            if (x === i)
-                x = true;
-            else
-                x = false;
-
-            rows.push(<tr className="game-row">{this.renderColumns(length, x, y)}</tr>);
-        }
-
-        return rows;
-    }
-
-    render() {
-
-        const board = this.props.board;
-        const length = board.length;
+            return(
+                <tr>
+                    {rows}
+                </tr>
+            );
+        });
 
         return(
             <table className="game-board">
-                {this.renderRows(length, 0, 0)}
+                {boardTable}
             </table>
+        )
+    }
+
+    render() {
+        return(
+            this.renderBoard()
         );
     }
 }
