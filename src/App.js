319da65ba7df34d2ca13ddbd249b349b070e9544
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-06 20:39:18 -0400

    Calculate and render updated board

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends React.Component {
             boardLength: 10,
             boardRandomness: 50,
             boardSpeed: 30,
+            timer: null,
             boardPieces: []
         };
     }
@@ -46,7 +47,7 @@ class App extends React.Component {
         })
     }
 
-    createBoardArray() {
+    createBoard(){
         let collumns = [];
         let board = [];
 
@@ -58,17 +59,110 @@ class App extends React.Component {
             board.push(collumns);
         }
 
+        return board;
+    }
+
+    createBoardArray() {
+        
+        let board = this.createBoard();
+
         this.randomlyPopulateArray(board);
+    }
+
+    returnCellValue(board, row, col){
+
+        let length = this.state.boardPieces.length;
+
+        if (row < 0){
+            row = length - 1;
+            col = (length - 1) - col;
+        }
+        if (row > length - 1){
+            row = 0;
+            col = (length - 1) - col;
+        }
+        if (col < 0){
+            row = (length - 1) - row;
+            col = length - 1;
+        }
+        if (col > length - 1){
+            row = (length - 1) - row;
+            col = 0;
+        }
+        
+
+        return board[row][col];
+    }
+
+    countsCellNeighbours(board, row, col){
+        const upperLeftCell    = this.returnCellValue(board, row - 1, col - 1);
+        const upperMiddleCell  = this.returnCellValue(board, row - 1, col);
+        const upperRightCell   = this.returnCellValue(board, row - 1, col + 1);
+
+        const centerLeftCell   = this.returnCellValue(board, row, col - 1);
+        const centerRightCell  = this.returnCellValue(board, row, col + 1);
+
+        const bottonLeftCell   = this.returnCellValue(board, row + 1, col - 1);
+        const bottonMiddleCell = this.returnCellValue(board, row + 1, col);
+        const bottonRightCell  = this.returnCellValue(board, row + 1, col + 1);
+
+        return (
+            upperLeftCell +
+            upperMiddleCell + 
+            upperRightCell +
+            centerLeftCell +
+            centerRightCell +
+            bottonLeftCell +
+            bottonMiddleCell +
+            bottonRightCell
+        );
+    }
+
+    determinesNextCellState(currentCell, numberOfNeighbours){
+
+        if (currentCell){
+            
+            if (numberOfNeighbours < 2)
+                return false;
+        
+            if (numberOfNeighbours === 2 || numberOfNeighbours === 3)
+                return true;
+
+            if (numberOfNeighbours > 3)
+                return false;
+        }
+        else {
+            if (numberOfNeighbours === 3)
+                return true;
+            else   
+                return false;
+        }
     }
 
     updateBoardArray(){
 
+        console.log("test");
+
         let board = this.state.boardPieces.slice();
 
-        if (board)
+        let newBoard = this.createBoard();
+
+        if (!board)
             return;
         
-        let curCellPos = [0,0];
+        for (let row = 0; row < board.length; row++){
+            for (let col = 0; col < board.length; col++){
+
+                const currentCell = this.returnCellValue(board, row, col);
+                const totalCellNeighbourhood = this.countsCellNeighbours(board, row, col);
+
+                newBoard[row][col] = this.determinesNextCellState(currentCell, totalCellNeighbourhood);
+            }
+        }
+
+        this.setState({
+            boardPieces: newBoard
+        });
     }
 
     handlelengthChange(e) {
@@ -144,7 +238,6 @@ class App extends React.Component {
 
                 <GameBoard 
                     isPlaying={this.state.isPlaying}
-                    speed={this.state.boardSpeed}
                     board={this.state.boardPieces}/>
 
                 <GameSlider 
